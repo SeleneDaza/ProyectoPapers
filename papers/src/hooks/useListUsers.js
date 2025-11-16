640d263be176c174803add6d9fc053d86566bcf7
Valeriatm06 <valeria.tocarruncho@uptc.ecu.co>
2025-11-16 08:23:04 -0500

    mejora en el hook de usuarios

--- papers/src/hooks/useListUsers.js
+++ papers/src/hooks/useListUsers.js
@@ -1,27 +1,36 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/usuarios';
+const API_URL = 'http://localhost:8080/api/users'; 
 
-export function useListUsers() {
+/**
+ * Hook para listar Usuarios.
+ * @param {string | null} roleFilter
+ * (ej. "CLIENTE", "PROVEEDOR")
+ */
+export function useListUsers(roleFilter = null) {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [roleFilter]);
 
   const fetchUsers = async () => {
     setLoading(true);
     try {
       const response = await axios.get(API_URL);
+      
+      let data = response.data;
 
-      const clients = response.data.filter(t => t.roles.includes('CLIENTE'));
-      setUsers(clients);
+      if (roleFilter) {
+        data = data.filter(t => t.roles.includes(roleFilter));
+      }
+      
+      setUsers(data);
 
     } catch (err) {
-      console.error("Error al cargar los usuarios:", err);
       setError(err);
     } finally {
       setLoading(false);
